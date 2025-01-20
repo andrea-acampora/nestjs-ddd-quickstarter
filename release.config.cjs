15a6446f94e7f7e97c7905158a8a7e5e1fbf7b6e
Andrea Acampora <andrea.acampora@protonmail.com>
2025-01-20 15:06:05 +0100

    chore: rename semantic release config file

--- release.config.cjs
+++ release.config.cjs
@@ -1,4 +1,4 @@
-import config from 'semantic-release-preconfigured-conventional-commits';
+const config = require('semantic-release-preconfigured-conventional-commits');
 
 const publishCommands = `
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md || exit 2
@@ -6,8 +6,8 @@ git push --force origin \${nextRelease.version} || exit 3
 echo "release_status=released" >> $GITHUB_ENV
 echo "CONTAINER_VERSION="\${nextRelease.version} >> $GITHUB_ENV
 `;
-const releaseBranches = ['main'];
 
+const releaseBranches = ['main'];
 config.branches = releaseBranches;
 
 config.plugins.push(
